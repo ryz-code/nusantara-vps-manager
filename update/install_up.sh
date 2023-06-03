182523af301784f7d199c71d768bf510ba007b52
ryzXD <111454488+ryz-code@users.noreply.github.com>
2023-06-04 03:08:57 +0700

    Add files via upload

--- update/install_up.sh
+++ update/install_up.sh
@@ -2,12 +2,11 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 echo -e "[INFO] Remove Old Script"
-rm -rf /usr/bin/m-ssh
-rm -rf /opt/.ver
+rm -rf /usr/bin/m-bot
 sleep 2
 echo -e "[INFO] Downloading New Script"
-wget -q -O /usr/bin/m-ssh "https://raw.githubusercontent.com/ryz-code/nusantara-vps-manager/autoscript/menu/m-ssh.sh" && chmod +x /usr/bin/m-ssh
-wget -q -O /opt/.ver "https://raw.githubusercontent.com/ryz-code/nusantara-vps-manager/autoscript/update/version_up" && chmod +x /opt/.ver
+wget -q -O /usr/bin/m-bot "https://raw.githubusercontent.com/ryz-code/nusantara-vps-manager/autoscript/menu/m-bot.sh" && chmod +x /usr/bin/m-bot
+wget -q -O /usr/bin/bot "https://raw.githubusercontent.com/ryz-code/nusantara-vps-manager/autoscript/bot/bot.sh" && chmod +x /usr/bin/bot
 echo -e "[INFO] Update Successfully"
 
 rm install_up.sh
