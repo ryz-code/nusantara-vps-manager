a07a0af6c40e27da5e3b9070f04f6062aec64f22
ryzXD <111454488+ryz-code@users.noreply.github.com>
2023-06-03 23:58:26 +0700

    Add files via upload

--- update/install_up.sh
+++ update/install_up.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-rm /usr/bin/menu
 echo -e "[INFO] Remove Old Script"
-
+rm /usr/bin/menu
 sleep 2
 echo -e " [INFO] Downloading New Script"
-wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/godtrex99/update/main/update_file/menu" && chmod +x /usr/bin/menu
-wget -qc -o /opt/.ver https:https://raw.githubusercontent.com/godtrex99/update/main/ver && chmod +x /opt/.ver
+wget -qc -O /usr/bin/menu "https://raw.githubusercontent.com/ryz-code/nusantara-vps-manager/autoscript/menu/menu.sh" && chmod +x /usr/bin/menu
+wget -qc -o /opt/.ver https:https://raw.githubusercontent.com/ryz-code/nusantara-vps-manager/autoscript/update/version && chmod +x /opt/.ver
 echo -e " [INFO] Update Successfully"
 
 rm install_up.sh
